43d06dce8dcf462bee81936e93f14cac6b0051a7
Ryan Manchester <ryanm17@gmail.com>
2017-12-12 03:59:19 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,8 +3,13 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
+  attr_accessor :email
 
-  def parse(email)
+  def initialize(email)
+    @email = email 
+  end
+
+  def parse
     email.split(/\,\s/)
   end
 
